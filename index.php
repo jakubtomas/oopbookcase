33e767c64f5da7128eae9dd047c03f62a76660a5
Jakub Tomas <tomasjakub21@gmail.com>
2020-01-02 22:48:02 +0100

    add footer and header and style.css

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 session_start();
 
-include 'load.php';
+include_once 'load.php';
+
+include_once '_partials/header.php';
 
 
 $db = new database();
 
 $user = new  users();
 
- $db->getconn();
+$db->getconn();
 echo 'hell';
 
 echo '<pre>';
@@ -16,8 +18,25 @@ echo '<pre>';
 echo '</pre>';
 
 
-echo '<pre>';
-print_r($user->getuser());
-echo '</pre>';
-;
 //$user->getdata();
+?>
+
+    <div class="white min-vh-100">
+
+        <h1>Hellou</h1>
+
+        <?php
+        echo '<pre>';
+        print_r($user->getuser());
+        echo '</pre>';
+
+        ?>
+
+
+    </div>
+
+
+<?php
+include_once '_partials/footer.php';
+
+?>
